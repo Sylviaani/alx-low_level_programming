11235df6bbf33bd35ff8dc2ea338de9162f612d2
Sylviaani <sylviaani26@gmail.com>
2023-04-21 20:18:53 -0700

    commit message

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,19 +5,19 @@
  */
 int main(void)
 {
-	int n, m, I;
+	int n, m, l;
 
 	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for (I = 50; I < 58; I++)
+			for (l = 50; l < 58; l++)
 			{
-				if (I > m && m > n)
+				if (l > m &&  m > n)
 				{
 					putchar(n);
 					putchar(m);
-					putchar(I);
+					putchar(l);
 					if (n != 55 || m != 56)
 					{
 						putchar(',');
